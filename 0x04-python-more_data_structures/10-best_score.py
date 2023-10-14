c47461fe5f9f3f4643492564151aa507b145e57b
Younessboumlik <younessboumlik1949@gmail.com>
2023-10-14 02:49:58 +0100

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if len(my_dict) == 0:
+        return None
+    max = 0
+    for i in a_dictionary:
+        if (a_dictionary[i] > max):
+            max = a_dictionary[i]
+    return max
