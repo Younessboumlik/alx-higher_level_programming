861a9210ef1db115e4c0bd9e67509cc75e3c90ed
Younessboumlik <younessboumlik1949@gmail.com>
2023-10-14 02:57:14 +0100

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if len(my_dict) == 0:
+    if not (a_dictionary):
         return None
     max = 0
+    name = ""
     for i in a_dictionary:
         if (a_dictionary[i] > max):
+            name = i
             max = a_dictionary[i]
-    return max
+    return name
