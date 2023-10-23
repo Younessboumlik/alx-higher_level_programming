5961bc09287569f4dec2d6658d1db727ad2c0853
Younessboumlik <younessboumlik1949@gmail.com>
2023-10-23 18:29:16 +0100

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,10 @@ def safe_print_list(my_list=[], x=0):
     try:
         i = 0
         while(i < x):
-            print(my_list[i])
+            print(my_list[i], end='')
             i += 1
+        print(end='\n')
         return(i)
-    except: Exception
+    except Exception:
+        print(end='\n')
+        return(i)
