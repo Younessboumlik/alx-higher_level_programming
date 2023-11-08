cec06f277f826c5098fd36b70c71605aa5a71d88
Youness Boumlik <younessboumlik@gmail.com>
2023-11-08 10:14:11 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@ reading a file.
 def read_file(filename=""):
     """print the content of filename."""
     with open(filename,encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(),end='')
         f.close()
