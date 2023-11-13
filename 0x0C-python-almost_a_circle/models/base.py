9d4aeca0116fc3998da13167e20347402840cded
Youness Boumlik <younessboumlik@gmail.com>
2023-11-13 21:14:52 +0100

    base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
 
     def __init__(self, id=None):
         """the init method."""
-        if id is not in None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects = Base.__nb_objects + 1
