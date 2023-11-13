b97f5c07cd53bdacbe25d14be9f3ef3f573b9c74
Youness Boumlik <younessboumlik@gmail.com>
2023-11-14 00:26:33 +0100

    task 0

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Rectangle(Base):
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """init method."""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Set the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
