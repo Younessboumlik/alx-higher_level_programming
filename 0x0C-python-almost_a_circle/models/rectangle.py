3b6a1f047e5cae7db0acc88ed2eed67ed68b70b1
Youness Boumlik <younessboumlik@gmail.com>
2023-11-14 12:44:27 +0100

    rectangle.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -96,11 +96,12 @@ class Rectangle(Base):
 
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
 
     def to_dictionary(self):
         """return a dict."""
-        dic=dict()
+        dic = dict()
         dic = {
             "id": self.id,
             "width": self.width,
