a1894a3e8a6e4dd614560ff1df106f1d79ca22ce
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-01-15 03:26:44 +0100

    task2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""  lists all states"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+  db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+  cursor = db.cursor()
+  cursor.execute("SELECT * FROM states WHERE name = %s",(sys.argv[4],))
+  result = cursor.fetchall()
+  for i in result:
+    print(i)
+  cursor.close()
+  db.close()
