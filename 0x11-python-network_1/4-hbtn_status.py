71c12e324767371e92e568c75cd1d2a37149fd7c
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-01-31 03:09:19 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""this is a simple program getting informations of a connection using requestes."""
+"""this is a simple program getting informations of a connection using requestes.
+"""
+
 import requests
+
 if __name__ == "__main__":
-        request =  requests.get("https://alx-intranet.hbtn.io/status")
-        print("Body response:")
-        print("\t- type:",type(request.text))
-        print("\t- content:",request.text)
+    request =  requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type:", type(request.text))
+    print("\t- content:", request.text)
