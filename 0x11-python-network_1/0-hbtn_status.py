b8bb8abbba82a7b24bd0be7d7d3f0a993d0af598
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-01-31 02:41:41 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Fetches alx status using urllib
+"""fetches alx status using urllib
 """
 
 if __name__ == '__main__':
@@ -10,6 +9,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
-        print("\t- type: ", type(body))
-        print("\t- content: ", body)
-        print("\t- utf8 content: ", body.decode("utf-8"))
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
