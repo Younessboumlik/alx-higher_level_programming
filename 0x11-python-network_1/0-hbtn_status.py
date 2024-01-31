7b5cf481cfc3c85e5d0b6856f0229269cd6decdd
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-01-31 02:22:04 +0100

    rask0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """this is a simple program getting informations of a connection using urllib."""
 
-import urllib.request
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type:",type(body))
-        print("\t- content:",body)
-        print("\t- utf8 content:",body.decode("utf-8"))
+if __name__ == '__main__':
+
+        import urllib.request
+        
+        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+                body = response.read()
+                print("Body response:")
+                print("\t- type:",type(body))
+                print("\t- content:",body)
+                print("\t- utf8 content:",body.decode("utf-8"))
