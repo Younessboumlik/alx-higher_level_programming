bcea51bc1896f3432f30c61520bd9a4309aaca0e
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-04-16 13:37:40 +0100

    task3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -18,15 +18,6 @@ class Rectangle{
       }
       console.log(line.slice(1));
     }
-    rotate(){
-        temp = this.height;
-        this.height = this.width;
-        this.width = temp;
-    }
-    double(){
-        this.height = this.height * 2;
-        this.width = this.width * 2;
-    }
   }
 
 module.exports = Rectangle;
