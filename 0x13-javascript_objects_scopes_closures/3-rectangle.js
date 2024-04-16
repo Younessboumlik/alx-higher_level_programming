5cb4c22a0e0a8bcaa35e5967e1b83f822d78fc15
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-04-16 13:31:38 +0100

    task3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,23 @@
 #!/usr/bin/node
 
 class Rectangle{
-  constructor(w,h){
-    if(w > 0 && h > 0){
-    this.width = w;
-    this.height = h;
-    }
-  }
-  print(){
-    for(i = 0; i < w; i++){
-      for(j = 0;j < h; j++){
-          console.log('X');
+    constructor(w,h){
+      if(w > 0 && h > 0){
+      this.width = w;
+      this.height = h;
       }
     }
+    print(){
+      let i,j;
+      let line = ''
+      for(i = 0; i < this.width; i++){
+        line += '\n'
+        for(j = 0;j < this.height; j++){
+            line += 'X'
+        }
+      }
+      console.log(line);
+    }
   }
-}
+
 module.exports = Rectangle;
